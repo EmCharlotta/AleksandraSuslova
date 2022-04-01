4aa4f9f4fd44eaace993e8d9e53c21b041cb897c
EmCharlotta <61659302+EmCharlotta@users.noreply.github.com>
2022-04-01 21:00:27 +0300

    JDI

--- src/main/java/com/epam/tc/jdi/site/pages/elements/Sets.java
+++ src/main/java/com/epam/tc/jdi/site/pages/elements/Sets.java
@@ -2,44 +2,39 @@ package com.epam.tc.jdi.site.pages.elements;
 
 
 public class Sets {
-    private DataSetForTest data_2;
-    private DataSetForTest data_1;
-    private DataSetForTest data_3;
-    private DataSetForTest data_4;
-    private DataSetForTest data_5;
+    private DataSetForTest data1;
+    private DataSetForTest data2;
+    private DataSetForTest data3;
+    private DataSetForTest data4;
+    private DataSetForTest data5;
 
-    public Sets(DataSetForTest data_1, DataSetForTest data_2, DataSetForTest data_3, DataSetForTest data_4,
-                DataSetForTest data_5) {
-        this.data_1 = data_1;
-        this.data_2 = data_2;
-        this.data_3 = data_3;
-        this.data_4 = data_4;
-        this.data_5 = data_5;
+    public Sets(DataSetForTest data1, DataSetForTest data2, DataSetForTest data3, DataSetForTest data4,
+                DataSetForTest data5) {
+        this.data1 = data1;
+        this.data2 = data2;
+        this.data3 = data3;
+        this.data4 = data4;
+        this.data5 = data5;
     }
 
     public DataSetForTest get1() {
-        return data_1;
+        return data1;
     }
 
     public DataSetForTest get2() {
-        return data_2;
+        return data2;
     }
 
     public DataSetForTest get3() {
-        return data_3;
+        return data3;
     }
 
     public DataSetForTest get4() {
-        return data_4;
+        return data4;
     }
 
     public DataSetForTest get5() {
-        return data_5;
+        return data5;
     }
 
-    @Override
-    public String toString() {
-        return "Sets{" + "data_1=" + data_1.toString() + ", data_2=" + data_2.toString() + ", data_3="
-            + data_3.toString() + ", data_4=" + data_4.toString() + ", data_5=" + data_5.toString() + '}';
-    }
 }
